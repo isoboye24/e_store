3a9b65c1e872d362c75e436249ccb21bf31d73a0
isoboye24 <isobarguy4real@gmail.com>
2025-08-14 03:34:01 +0200

    I completed the navbar component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+// import { LuArmchair } from 'react-icons/lu';
+import { FaShopware } from 'react-icons/fa6';
 
-const Logo = () => {
-  return <div>Logo</div>;
-};
+function Logo() {
+  return (
+    <Button
+      size="icon"
+      asChild
+      className="bg-orange-500 hover:bg-orange-600 transition ease-in-out duration-300"
+    >
+      <Link href="/">
+        <FaShopware className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
+}
 
 export default Logo;
